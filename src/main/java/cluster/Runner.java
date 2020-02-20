f1fe83c54c4eaf7b4d097c43816b7ca9fae754ee
Hugh McKee <mckee.hugh@gmail.com>
2020-02-20 07:18:57 -0500

    multiple tweaks and small fixes. add localhost2 script

--- src/main/java/cluster/Runner.java
+++ src/main/java/cluster/Runner.java
@@ -29,13 +29,17 @@ public class Runner {
     }
 
     private static Config setupClusterNodeConfig(String port) {
-        String hostIp = port.compareTo("2555") <= 0 ? "127.0.0.1" : "127.0.0.2";
+        final String hostname1 = "127.0.0.1";
+        final String hostname2 = "127.0.0.2";
+        final String hostname = port.compareTo("2555") <= 0 ? hostname1 : hostname2;
         return ConfigFactory
-                .parseString(String.format("akka.remote.artery.canonical.hostname = \"%s\"%n", hostIp)
+                .parseString(String.format("akka.remote.artery.canonical.hostname = \"%s\"%n", hostname)
                         + String.format("akka.remote.artery.canonical.port=%s%n", port)
                         + String.format("akka.management.http.hostname = \"%s\"%n", "127.0.0.1")
                         + String.format("akka.management.http.port=%s%n", port.replace("255", "855"))
-                        + String.format("akka.management.http.route-providers-read-only = %s%n", "false"))
+                        + String.format("akka.management.http.route-providers-read-only = %s%n", "false")
+                        + String.format("akka.remote.artery.advanced.tcp.outbound-client-hostname = %s%n", hostname))
                 .withFallback(ConfigFactory.load());
     }
 }
+
