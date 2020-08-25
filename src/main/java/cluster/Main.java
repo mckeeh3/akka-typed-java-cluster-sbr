82aca7b0ae3d7e5066b64b3981ad4cc8f7360174
Hugh McKee <mckeeh3@gmail.com>
2020-08-25 08:06:55 -0400

    update code

--- src/main/java/cluster/Main.java
+++ src/main/java/cluster/Main.java
@@ -1,13 +1,15 @@
 package cluster;
 
-import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.Terminated;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
-import akka.cluster.ClusterEvent;
-import akka.cluster.typed.Cluster;
-import akka.cluster.typed.Subscribe;
+import akka.management.javadsl.AkkaManagement;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import java.util.Arrays;
 
 class Main {
   static Behavior<Void> create() {
@@ -19,7 +21,30 @@ class Main {
   }
 
   private static void bootstrap(final ActorContext<Void> context) {
-    final ActorRef<ClusterEvent.ClusterDomainEvent> clusterListener =
-        context.spawn(ClusterListenerActor.create(), "clusterListener");
+    context.spawn(ClusterListenerActor.create(), "clusterListener");
+  }
+
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      throw new RuntimeException("Akka node port is required.");
+    }
+    final String port = Arrays.asList(args).get(0);
+    final ActorSystem<?> actorSystem = ActorSystem.create(Main.create(), "cluster", setupClusterNodeConfig(port));
+    AkkaManagement.get(actorSystem.classicSystem()).start();
+    HttpServer.start(actorSystem);
+  }
+
+  private static Config setupClusterNodeConfig(String port) {
+    final String localhost1 = "127.0.0.1";
+    final String localhost2 = "127.0.0.2";
+    final String hostname = port.compareTo("2555") <= 0 ? localhost1 : localhost2;
+    return ConfigFactory
+        .parseString(String.format("akka.remote.artery.canonical.hostname = \"%s\"%n", hostname)
+            + String.format("akka.remote.artery.canonical.port=%s%n", port)
+            + String.format("akka.management.http.hostname = \"%s\"%n", "127.0.0.1")
+            + String.format("akka.management.http.port=%s%n", port.replace("255", "855"))
+            + String.format("akka.management.http.route-providers-read-only = %s%n", "false")
+            + String.format("akka.remote.artery.advanced.tcp.outbound-client-hostname = %s%n", hostname))
+        .withFallback(ConfigFactory.load());
   }
 }
